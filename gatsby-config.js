59491c7299df88d53500b51ce5ab8bba0f6a94a6
Arthur Violy <arthur@violy.net>
2021-09-30 21:35:58 +0200

    gh-pages - 30 sept. 2021

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/l2lcn-ecritures-numeriques",
   siteMetadata: {
     title: `L2 Lettres, Création et Numérique (LCN)`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
